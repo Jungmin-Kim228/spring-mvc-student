2fa63565c1bf4a753d7c32508f2327e2378a069d
dgb980228 <dgb980228@naver.com>
2022-05-03 00:02:28 +0900

    success register validation exception

--- spring-mvc-student/src/main/java/com/nhnacademy/student/controller/StudentRegisterController.java
+++ spring-mvc-student/src/main/java/com/nhnacademy/student/controller/StudentRegisterController.java
@@ -2,9 +2,15 @@ package com.nhnacademy.student.controller;
 
 import com.nhnacademy.student.domain.Student;
 import com.nhnacademy.student.domain.StudentRegisterRequest;
+import com.nhnacademy.student.exception.ValidationFailedException;
 import com.nhnacademy.student.repository.StudentRepository;
+import com.nhnacademy.student.validator.StudentRegisterRequestValidator;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +21,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class StudentRegisterController {
     private final StudentRepository studentRepository;
 
-    public StudentRegisterController(StudentRepository studentRepository) {
+    private final StudentRegisterRequestValidator validator;
+
+    public StudentRegisterController(StudentRepository studentRepository, StudentRegisterRequestValidator validator) {
         this.studentRepository = studentRepository;
+        this.validator = validator;
     }
 
     @GetMapping
@@ -25,8 +34,11 @@ public class StudentRegisterController {
     }
 
     @PostMapping
-    public ModelAndView registerStudent(@ModelAttribute StudentRegisterRequest studentRequest) {
-        // Todo validated and binding result
+    public ModelAndView registerStudent(@Valid @ModelAttribute StudentRegisterRequest studentRequest,
+                                        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationFailedException(bindingResult);
+        }
 
         Student student =
             studentRepository.register(studentRequest.getName(), studentRequest.getEmail(),
@@ -38,4 +50,8 @@ public class StudentRegisterController {
         return mav;
     }
 
+    @InitBinder("studentRegisterRequest")
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(validator);
+    }
 }
