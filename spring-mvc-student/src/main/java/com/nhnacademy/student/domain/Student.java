01c3a16d78ef0a2072e68c1f4708e74cde6f46be
dgb980228 <dgb980228@naver.com>
2022-05-02 19:07:45 +0900

    init project

---
+++ spring-mvc-student/src/main/java/com/nhnacademy/student/domain/Student.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.student.domain;
+
+public class Student {
+    private long id;
+    private String name;
+    private String email;
+    private int score;
+    private String comment;
+
+}
